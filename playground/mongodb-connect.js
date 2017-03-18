a4d4d4d5033c470bebbb9c7f319041b4bcdf7c92
Tri <caominhtri91@gmail.com>
2017-03-18 23:20:45 +0700

    fix all

--- playground/mongodb-connect.js
+++ playground/mongodb-connect.js
@@ -4,8 +4,8 @@ MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
 	if (err) {
 		return console.log('Unable to connect to MongoDB server');
 	}
-	
-	console.log('Connected to MongoDB server');
+
+	console.log('Connected to MongoDB server')
 
 	// db.collection('Todos').insertOne({
 	// 	text: 'Something to do',
